ff257db8cd23ae0198c70e4dd8bb33838d0ace71
dom <davide.domini@unibo.it>
2024-01-10 08:51:46 +0100

    fix(AggregateLogic): first collect all the models then do the fusion

--- src/main/scala/it/unibo/AggregateLogic.scala
+++ src/main/scala/it/unibo/AggregateLogic.scala
@@ -10,9 +10,12 @@ class AggregateLogic
 
   override def main(): Unit = {
     val initialModel = Seq[Double]() // TODO - this should be the real model :)
+
     rep(initialModel) { m =>
       val newModel = learn(m)
-      val aggregatedModel = foldhoodPlus(newModel)((a,b) => modelsFusion(a,b))(nbr(newModel))
+      val neighborsModels =
+        foldhood(Seq[Seq[Double]]())(_ ++ _)(nbr(Seq(newModel)))
+      val aggregatedModel = modelsFusion(neighborsModels)
       val eval = evaluate(aggregatedModel)
       //data export
       node.put("Accuracy", eval.accuracy)
@@ -22,7 +25,7 @@ class AggregateLogic
   }
 
   // TODO - implement {full average, mutual knowledge transfer} algorithm
-  private def modelsFusion(m1: Seq[Double], m2: Seq[Double]): Seq[Double] = ???
+  private def modelsFusion(models: Seq[Seq[Double]]): Seq[Double] = ???
 
   // TODO - implement SDG
   private def learn(m: Seq[Double]): Seq[Double] = ???
