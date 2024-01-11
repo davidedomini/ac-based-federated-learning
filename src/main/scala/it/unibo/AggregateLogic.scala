de19bb2c10c9a84e7ff415affb30dbea9e4d3cec
dom <davide.domini@unibo.it>
2024-01-11 16:31:13 +0100

    feat: implement model fusion

--- src/main/scala/it/unibo/AggregateLogic.scala
+++ src/main/scala/it/unibo/AggregateLogic.scala
@@ -2,6 +2,7 @@ package it.unibo
 
 import it.unibo.alchemist.model.scafi.ScafiIncarnationForAlchemist._
 import me.shadaj.scalapy.py
+import PythonModules._
 
 class AggregateLogic
   extends AggregateProgram
@@ -9,8 +10,12 @@ class AggregateLogic
     with ScafiAlchemistSupport
     with FieldUtils {
 
+  private val input = 5
+  private val hidden = 20
+  private val output = 3
+
   override def main(): Unit = {
-    val initialModel = SimpleNN(5, 20, 3) // TODO
+    val initialModel = SimpleNN(input, hidden, output) // TODO
 
     rep(initialModel) { m =>
       val newModel = learn(m)
@@ -26,22 +31,20 @@ class AggregateLogic
   }
 
   private def modelsFusion(models: Seq[py.Dynamic]): py.Dynamic = {
-    val summedModels = models.reduce(elementByElementSum)
-    val averageModel = avgModel(summedModels, models.size)
-    averageModel
+    val w_avg = utils.average_weights(models.toPythonProxy)
+    val am = SimpleNN(input, hidden, output) // fresh network
+    am.load_state_dict(w_avg)
+    am
   }
 
   private def modelSampling(model: py.Dynamic): py.Dynamic = {
     model.state_dict()
   }
 
-  // TODO
-  private def elementByElementSum(m1: py.Dynamic, m2: py.Dynamic): py.Dynamic = ???
-  private def avgModel(m: py.Dynamic, k: Int): py.Dynamic = ???
-
   // TODO - implement SDG
-  private def learn(m: py.Dynamic): py.Dynamic = ???
+  private def learn(m: py.Dynamic): py.Dynamic = m
 
   // TODO
-  private def evaluate(m: py.Dynamic): Evaluation = ???
+  private def evaluate(m: py.Dynamic): Evaluation = Evaluation(1.0, 0.0)
+
 }
