a3585f6a38dc986d9b9a8f40ca411098411e8b97
dom <davide.domini@unibo.it>
2024-01-18 12:24:49 +0100

    fix: fix when a python function returns more than one value

--- src/main/scala/it/unibo/AggregateLogic.scala
+++ src/main/scala/it/unibo/AggregateLogic.scala
@@ -2,6 +2,7 @@ package it.unibo
 
 import it.unibo.alchemist.model.scafi.ScafiIncarnationForAlchemist._
 import me.shadaj.scalapy.py
+import me.shadaj.scalapy.py.PyQuote
 import PythonModules._
 
 class AggregateLogic
@@ -14,11 +15,11 @@ class AggregateLogic
   private val hidden = 20
   private val output = 3
   private val epochs = 5
-  private val initialModel = SimpleNN(input, hidden, output) // TODO - MNIST CNN
+  private val initialModel = MNISTNN(1, 10)
 
   override def main(): Unit = {
 
-    val (trainDataset, testDataset, dataDivision) = Utils.getDataset(mid()) // TODO - li ricarica ogni volta?
+    val (trainDataset, testDataset, dataDivision) = Utils.getDataset(mid()) // TODO - could be a performance issue
 
     rep(initialModel) { m =>
       val newModel = learn(m, trainDataset, dataDivision)
@@ -27,8 +28,11 @@ class AggregateLogic
       val aggregatedModel = modelsFusion(neighborsModels)
       val eval = evaluate(aggregatedModel, testDataset, dataDivision)
       //data export
-      node.put("Accuracy", eval.accuracy)
-      node.put("Loss", eval.loss)
+      // TODO - maybe better data could be exported
+      val accuracy = py"$eval[0]"
+      val loss = py"$eval[1]"
+      node.put("Accuracy", accuracy)
+      node.put("Loss", loss)
       aggregatedModel
     }
   }
@@ -46,17 +50,19 @@ class AggregateLogic
 
   private def learn(model: py.Dynamic, trainDataset: py.Dynamic, dataDivison: py.Dynamic): py.Dynamic = {
     val trainloader = utils.train_data_loader(trainDataset, dataDivison)
-    val newWeights, loss = utils.update_weigths(model, epochs, trainloader, "cpu")
+    val result = utils.update_weigths(model, epochs, trainloader, "cpu") // result = (newWeights, loss)
+    val newWeights = py"$result[0]"
+    val loss = py"$result[1]"
     // TODO - log loss
-    val newModel = SimpleNN(input, hidden, output)
+    val newModel =  MNISTNN(1, 10)
     newModel.load_state_dict(newWeights)
     newModel
   }
   
-  private def evaluate(model: py.Dynamic, dataset: py.Dynamic, dataDivison: py.Dynamic): Evaluation = {
+  private def evaluate(model: py.Dynamic, dataset: py.Dynamic, dataDivison: py.Dynamic): py.Dynamic = {
     val validloader = utils.val_data_loader(dataset, dataDivison)
-    val accuracy, loss = utils.evaluate(model, validloader, "cpu")
-    Evaluation(1.0, 0.0) // TODO - understand how to convert type of accuracy and loss from Dynamic to Double
+    val evaluationResult = utils.evaluate(model, validloader, "cpu")
+    evaluationResult
   }
 
 }
