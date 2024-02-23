cd8650780aabcd607480f2b29164d9d901304a77
dom <davide.domini@unibo.it>
2024-02-23 15:04:57 +0100

    feat: add param to check if mnist or fashionmnist

--- src/main/scala/it/unibo/mnist/Utils.scala
+++ src/main/scala/it/unibo/mnist/Utils.scala
@@ -1,31 +1,53 @@
 package it.unibo.mnist
 
 import it.unibo.common.PythonModules._
+import me.shadaj.scalapy.py
 
 object Utils {
 
-  def getDataset(id: Int) = {
+  def getDataset(id: Int, experiment: Int) = {
     val apply_transform = torchvision.transforms.ToTensor()
-    val trainDataset = torchvision
-      .datasets
-      .MNIST(
-        "data/mnist/",
-        train = true,
-        download = true,
-        transform = apply_transform
-      )
+    var trainDataset: py.Dynamic = null
+    var testDataset: py.Dynamic = null
+    if(experiment == 0){
+      trainDataset = torchvision
+        .datasets
+        .MNIST(
+          "data/mnist/",
+          train = true,
+          download = true,
+          transform = apply_transform
+        )
 
-    val testDataset = torchvision
-      .datasets
-      .MNIST(
-        "data/mnist/",
-        train = true,
-        download = true,
-        transform = apply_transform
-      )
+      testDataset = torchvision
+        .datasets
+        .MNIST(
+          "data/mnist/",
+          train = true,
+          download = true,
+          transform = apply_transform
+        )
+    } else {
+      trainDataset = torchvision
+        .datasets
+        .FashionMNIST(
+          "data/mnist/",
+          train = true,
+          download = true,
+          transform = apply_transform
+        )
+
+      testDataset = torchvision
+        .datasets
+        .FashionMNIST(
+          "data/mnist/",
+          train = true,
+          download = true,
+          transform = apply_transform
+        )
+    }
 
     val dataDivision = utils.load_data_division("data/data_division_iid.json", id)
-
     (trainDataset, testDataset, dataDivision)
   }
 
