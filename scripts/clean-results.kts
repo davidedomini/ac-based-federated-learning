673a5ae7b1c5803dd7df193af965e3e5c4c019e5
dom <davide.domini@unibo.it>
2024-02-23 12:10:56 +0100

    feat: add kt script to clean up alchemist results csv

---
+++ scripts/clean-results.kts
@@ -0,0 +1,29 @@
+#!/usr/bin/env kotlin
+
+import java.io.File
+
+data class SimulationResult(val path: String, val content: List<String>)
+
+fun clean(lines: List<String>): List<String> {
+    return lines
+        .filter { !it.startsWith("#") || it.contains("max") }
+        .filter { !it.contains("NaN") }
+        .map { if (it.startsWith("#")) it.substringAfter("# ") else it }
+}
+
+val dir = "../alchemist-data/"
+val paths = listOf(
+    dir + "simulation_MNIST.csv",
+    dir + "simulation.csv"
+)
+
+paths
+    .map { File(it) }
+    .map { SimulationResult(it.path, it.readLines()) }
+    .map { SimulationResult(it.path, clean(it.content)) }
+    .forEach {
+        val prefix = it.path.substringBeforeLast("/")
+        val postfix = it.path.substringAfterLast("/")
+        File("${prefix}/cleaned-${postfix}")
+            .writeText(it.content.joinToString(separator = "\n"))
+    }
